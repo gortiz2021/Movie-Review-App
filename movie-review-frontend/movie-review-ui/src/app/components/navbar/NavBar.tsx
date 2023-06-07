cf63afdd6e9fe0c79a353f27e66edd499f8caf4f
Gabriel Ortiz <igabriel661@hotmail.com>
2023-06-07 12:28:11 -0700

    Added styling to navbar.

--- movie-review-frontend/movie-review-ui/src/app/components/navbar/NavBar.tsx
+++ movie-review-frontend/movie-review-ui/src/app/components/navbar/NavBar.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='border-4 border-black m-2'>    
+    <div className='bg-slate-900 text-slate-50 px-2 py-4 m-2'>    
         <nav >
             <div className='flex justify-between'>
                 <div>
-                    <a href='' className='mx-2'>Reel Rater</a>
+                    <a href='' className='border-4 border-red-700 rounded-full bg-red-700 p-1 mx-2'>Reel Rater</a>
                 </div>
 
                 <div>
-                    <a href='' className='mx-2'>Login</a>                    
-                    <a href='' className='mx-2'>Register</a>
+                    <a href='' className='border-4 border-red-700 rounded-full bg-red-700 p-1 mx-2'>Login</a>                    
+                    <a href='' className='border-4 border-red-700 rounded-full bg-red-700 p-1 mx-2'>Register</a>
                 </div>  
             </div>
         </nav>
