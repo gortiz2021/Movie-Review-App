9449bf2c6f3f1342bede0c71c5105892dae9a970
Gabriel Ortiz <igabriel661@hotmail.com>
2023-06-10 01:11:53 -0700

    Added styling to review button.

--- movie-review-frontend/movie-review-ui/src/app/components/review/Review.tsx
+++ movie-review-frontend/movie-review-ui/src/app/components/review/Review.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Review = () => {
   return (
     <div className="bg-stone-800 text-white px-2 py-4 m-2">
-      <h1 >Review</h1>
+      <button type="submit" className="rounded-full bg-red-700 p-2 mx-2">
+        Review
+      </button>
     </div>
   );
 };
--- movie-review-frontend/movie-review-ui/src/app/components/trailer/Trailer.tsx
+++ movie-review-frontend/movie-review-ui/src/app/components/trailer/Trailer.tsx
@@ -6,10 +6,9 @@ const Trailer = () => {
   return (
     <div className="bg-stone-800 text-white px-2 py-4 m-2">
       <a className="text-red-700" href="">
-        <div className="text-sm">
-          <FontAwesomeIcon icon={faCirclePlay} className="w-10"/>
+        <div className="text-sm p-0 m-0">
+          <FontAwesomeIcon icon={faCirclePlay} className="w-10" />
         </div>
-        
       </a>
     </div>
   );
