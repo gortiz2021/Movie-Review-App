600742ca59bbf441d44749eb072a28ef33b93ce2
Gabriel Ortiz <igabriel661@hotmail.com>
2023-06-29 11:44:11 -0700

    Added trailers links to movie titles. Next is to display one movie at a time.

--- movie-review-frontend/movie-review-ui/src/app/components/home/Home.tsx
+++ movie-review-frontend/movie-review-ui/src/app/components/home/Home.tsx
@@ -5,36 +5,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Link from "next/link";
 config.autoAddCss = false;
 
-async function getMovieData() {
-  const response = await fetch(
-    "http://localhost:8080/api/v1/ReelRater/allMovies"
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return response.json();
-}
-
-export default async function Home() {
-  const movies = await getMovieData();
+export default async function Home({ movies }) {
 
   return (
     <div className="bg-stone-800 text-white px-2 py-4 m-2">
-      <button className="text-red-700 text-3xl">
+      {/* <button className="text-red-700 text-3xl">
         <FontAwesomeIcon icon={faCircleArrowLeft} />
-      </button>
-      <div className="flex">
+      </button> */}
+
+      <div className="flex overflow-x-auto">
         {movies.map((movie) => (
-          <h4 key={movie.imdbId} className="p-4">{movie.title}</h4>
+          <div key={movie.imdbId} className="p-4">    
+            <Link href={movie.trailerLink}>
+              {movie.title}
+            </Link>         
+          </div>
         ))}
       </div>
 
-      <button className="text-red-700 text-red-700 text-3xl">
+      {/* <button className="text-red-700 text-red-700 text-3xl">
         <FontAwesomeIcon icon={faCircleArrowRight} />
-      </button>
+      </button> */}
     </div>
   );
 }
--- movie-review-frontend/movie-review-ui/src/app/page.tsx
+++ movie-review-frontend/movie-review-ui/src/app/page.tsx
@@ -3,11 +3,37 @@ import NavBar from "./components/navbar/NavBar";
 import Review from "./components/review/Review";
 import Trailer from "./components/trailer/Trailer";
 
-export default function app() {
+type Movie = {
+  id: number;
+  imdbId: string;
+  title: string;
+  releaseDate: Date;
+  trailerLink: string;
+  poster: string;
+  genres: string;
+  backdrops: string;
+  reveiwIds: string;
+}
+
+async function getMovieData() {
+  const response = await fetch(
+    "http://localhost:8080/api/v1/ReelRater/allMovies"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+}
+
+export default async function app() {
+
+  const movies: Movie = await getMovieData();
+
   return (
     <div className="py-1">
       <NavBar />
-      <Home />
+      <Home movies={movies}/>
       <Trailer />
       <Review />
     </div>
